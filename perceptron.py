39ea65108b690d921fbf485706226b8c18501131
Alexander Ryndin <alexander.ryndin@rwth-aachen.de>
2016-01-30 20:31:47 +0100

    plot iris data set

--- perceptron.py
+++ perceptron.py
@@ -1,4 +1,6 @@
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
 class Perceptron(object):
@@ -28,3 +30,16 @@ class Perceptron(object):
 
     def predict(self, X):
         return np.where(self.net_input(X) >= 0.0, 1, -1)
+
+
+df = pd.read_csv('./iris.data', header=None)
+y = df.iloc[0:100, 4].values
+y = np.where(y == 'Iris-setosa', -1, 1)
+print(y)
+X = df.iloc[0:100, [0, 2]].values
+plt.scatter(X[:50, 0], X[:50, 1], color='red', marker='o', label='setosa')
+plt.scatter(X[50:100, 0], X[50:100, 1], color='blue', marker='x', label='versicolor')
+plt.xlabel('petal length')
+plt.ylabel('sepal length')
+plt.legend(loc='upper left')
+plt.show()
