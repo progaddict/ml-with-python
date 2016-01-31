ea68f5275f7202c683ccad6e8969e4fd83ae45f4
Alex Ryndin <ryndin.as@gmail.com>
2016-01-31 16:16:00 +0100

    small refactoring

--- classifying_iris_data_with_adaline.py
+++ classifying_iris_data_with_adaline.py
@@ -2,7 +2,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from mystuff import perceptron
+from mystuff import adaline
 from mystuff import plotting_stuff
 
 plt.close('all')
@@ -20,14 +20,14 @@ initial_data_plot.legend(loc='upper left')
 initial_data_plot.scatter(X[:50, 0], X[:50, 1], color='red', marker='o', label='setosa')
 initial_data_plot.scatter(X[50:100, 0], X[50:100, 1], color='blue', marker='x', label='versicolor')
 
-ppn = perceptron.Perceptron(eta=0.1, n_iter=10)
+ppn = adaline.AdalineGD(eta=0.1, n_iter=10)
 ppn.fit(X, y)
 fig2 = plt.figure()
 errors_plot = fig2.add_subplot(111)
 errors_plot.set_title('errors during fitting')
 errors_plot.set_xlabel('Epochs')
 errors_plot.set_ylabel('Number of misclassifications')
-errors_plot.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
+errors_plot.plot(range(1, len(ppn.cost_) + 1), ppn.cost_, marker='o')
 
 plotting_stuff.plot_decision_regions(X, y, classifier=ppn)
 
